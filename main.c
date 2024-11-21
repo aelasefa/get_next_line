9aa8b297cf3ec6f8990e6020787b727161ced11b
Ayman Elasefar <ayelasef@e1r9p7.1337.ma>
2024-11-21 13:52:49 +0100

    main

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include "get_next_line.h"
+#include <stdio.h>
 
-int main()
+int main(int ac, char **av)
 {
-	int fd = open("text.txt", O_RDONLY);
-	while (1)
+	int fd = open(av[ac-1], O_RDONLY);
+	int i=1;
+	char *line = get_next_line(fd);
+	while (line)
 	{
+        printf("[line %d] %s" , i, line);	
+		free(line);	
 		line = get_next_line(fd);
-		if (!line)
-			break;
-		printf("%s", line);
-		free(line);
+		i++;
+	
 	}
+	close(fd);
 }
